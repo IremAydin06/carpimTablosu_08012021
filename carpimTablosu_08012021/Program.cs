a9c5f6b8e0388c4cb2d7b04dc30b4d229b8de62b
İREM AYDIN <iaydin0698@hotmail.com>
2021-01-09 17:25:34 +0300

    Add project files.

---
+++ carpimTablosu_08012021/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carpimTablosu_08012021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a < 10; a++)
+            {
+                for (int b = 1; b < 10; b++)
+                {
+                    int carpim = a * b;
+                    Console.WriteLine($"{a} x {b} = {carpim}");
+                }
+                Console.WriteLine("-------------------");
+            }
+            Console.ReadLine();
+        }
+    }
+}
